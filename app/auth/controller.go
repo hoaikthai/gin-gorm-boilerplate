7f83333e13de845792be044ee66afce1d7f2ae33
Hoai Thai <hthai@openwt.com>
2020-02-19 17:50:53 +0700

    Validate SignUpForm after receiving

--- app/auth/controller.go
+++ app/auth/controller.go
@@ -1,10 +1,26 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Controller struct{}
 
-func (a Controller) SignUp(c *gin.Context) {}
+func (a Controller) SignUp(c *gin.Context) {
+	var signUpForm SignUpForm
+	if err := c.ShouldBindJSON(&signUpForm); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"success": true,
+	})
+	return
+}
 
 func (a Controller) SignIn(c *gin.Context) {}
 
