e974bfa04b293c37a4264d8dcaf34120d8b77e32
Hoai Thai <hthai@openwt.com>
2020-02-19 17:23:04 +0700

    Enhance validation

--- app/auth/forms.go
+++ app/auth/forms.go
@@ -1,12 +1,12 @@
 package auth
 
 type SignInForm struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email,max=100"`
+	Password string `json:"password" binding:"required,min=8"`
 }
 
 type SignUpForm struct {
-	Email    string `json:"email" binding:"required"`
-	UserName string `json:"username" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	UserName string `json:"username" binding:"required,max=20"`
+	Password string `json:"password" binding:"required,max=20,min=8"`
 }
